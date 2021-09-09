e3c9c897a55b5dcc9952fd0c2aead2c3bf94b884
WillUnger <williamkjunger@gmail.com>
2021-09-09 14:45:31 +0000

    add question array to game.js

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,4 +1,19 @@
 
+const questions = document.getElementById('question');
+
+
+
+
+              
+          
+
+      
+
+
+
+
+
+
 
 
 /**
@@ -6,6 +21,13 @@
 * and after the user's answer has been processed
 */
 function runGame(gameType) {}
+
+
+
+
+
+
+
 
 
 /**
@@ -43,4 +65,39 @@ function incrementScore() {
   }
 
 
-  const questions = []
+
+
+
+//questions
+let questions = [
+  {
+    numb: 1,
+    question : "1. Which one of these is NOT a moon of Jupiter?",
+    options : ["A. Calisto", "B. Io", "C. Europa", "D. Miranda"],
+    answer : "D. Miranda"
+  },
+  {
+    numb: 2,
+    question : "2. Which planet has the coolest atmosphere?",
+    options : ["A. Saturn", "B. Uranus", "C. Neptune", "D. Pluto"],
+    answer : "B. Uranus"
+  },
+  {
+    numb: 3,
+    question : "3. Which one of these planets rotates on its side?",
+    options : ["A. Saturn", "B. Uranus", "C. Neptune", "D. Jupiter"],
+    answer : "B. Uranus"
+  }, 
+  {
+    numb: 4,
+    question : "4. At the Sun’s Core, Nuclear Fusion converts Hydrogen to ________",
+    options : ["A. Plutonium", "B. Helium", "C. Potassium", "D. Radium"],
+    answer : "B. Helium"
+  }, 
+  {
+    numb: 5,
+    question : "5. The nearest star to our Solar System is?",
+    options : ["A. Epsilon Indi", "B. Proxima Centauri", "C. Sirius", "D. DX Cancri"],
+    answer : "B. Proxima Centauri"
+  },  
+]
